be8706dda7b4ce7ed4faf4f35cc7c867a1f2d040
Kelve Aragão <kelve.aragao@ifactory.com.br>
2018-10-29 18:39:22 -0300

    Adiciona preact

---
+++ src/components/header/index.js
@@ -0,0 +1,65 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style';
+
+const Header = () => (
+	<header>
+		<nav>
+			<div class="row">
+				<img
+					src="../assets/img/logo-white.png"
+					alt="Omnifood logo"
+					class="logo"
+				/>
+				<img
+					src="../assets/img/logo.png"
+					alt="Omnifood logo"
+					class="logo-black"
+				/>
+				<ul class="main-nav js--main-nav">
+					<li>
+						<a href="#features">Food delivery</a>
+					</li>
+					<li>
+						<a href="#works">How it works</a>
+					</li>
+					<li>
+						<a href="#cities">Our cities</a>
+					</li>
+					<li>
+						<a href="#plans">Sign up</a>
+					</li>
+				</ul>
+				<a class="mobile-nav-icon js--nav-icon">
+					<i class="ion-navicon-round" />
+				</a>
+			</div>
+		</nav>
+		<div class="hero-text-box">
+			<h1>
+				Goodbye junk food.
+				<br />
+				Hello super healthy meals.
+			</h1>
+			<a class="btn btn-full js--scroll-to-plans" href="#">
+				I'm hungry
+			</a>
+			<a class="btn btn-ghost js--scroll-to-start" href="#">
+				Show me more
+			</a>
+		</div>
+	</header>
+);
+
+export default Header;
+
+/*
+	<header class={style.header}>
+		<h1>Preact App</h1>
+		<nav>
+			<Link activeClassName={style.active} href="/">Home</Link>
+			<Link activeClassName={style.active} href="/profile">Me</Link>
+			<Link activeClassName={style.active} href="/profile/john">John</Link>
+		</nav>
+	</header>
+*/
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './assets/vendors/css/normalize.css';
+import './assets/vendors/css/grid.css';
+import './assets/vendors/css/ionicons.min.css';
+import './assets/vendors/css/animate.css';
+import './assets/resources/css/style.css';
+import './assets/resources/css/queries.css';
+
+// import './style';
+
+import App from './components/app';
+
+export default App;
